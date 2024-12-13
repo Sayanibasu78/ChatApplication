62392e4b034f68062bdacc6ede006efbb9e3664e
sayaniB <sayanibasu2232@gmail.com>
2024-12-13 23:59:10 +0530

    chat-application

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    swcPlugins: [
+      ["next-superjson-plugin", {}]
+    ]
+  },
   images: {
     domains: ['res.cloudinary.com'],
   },
